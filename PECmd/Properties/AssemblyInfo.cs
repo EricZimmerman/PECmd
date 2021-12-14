2e059b4fd8a4f96517bb7579265f8a48ef20d8c1
Eric Zimmerman <saericzimmerman@gmail.com>
2021-12-13 21:39:59 -0500

    modernize sln

--- PECmd/Properties/AssemblyInfo.cs
+++ PECmd/Properties/AssemblyInfo.cs
@@ -1,18 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
-[assembly: AssemblyTitle("PECmd")]
-[assembly: AssemblyDescription("Prefetch Explorer Command Line Edition")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Eric Zimmerman")]
-[assembly: AssemblyProduct("PECmd")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("saericzimmerman@gmail.com")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -23,17 +11,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("c928c55d-bec3-42cd-a775-bbb19c00f2bb")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.4.0.0")]
-[assembly: AssemblyFileVersion("1.4.0.0")]
